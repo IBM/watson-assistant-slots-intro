782098e665f9555068e968b0e2e308f357d60cc8
Scott D'Angelo <scott.dangelo@gmail.com>
2019-04-11 14:59:35 -0600

    Change to use IAM apikey and url. (#107)
    The K8s deploy used username and password for old style
    Watson assistant credentials.
    Switch to using IAM apikey and URL.

--- run-server.sh
+++ run-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
-export CONVERSATION_USERNAME=$(echo "$service_watson_conversation" | jq -r '.username')
-export CONVERSATION_PASSWORD=$(echo "$service_watson_conversation" | jq -r '.password')
+export CONVERSATION_IAM_APIKEY=$(echo "$service_watson_conversation" | jq -r '.apikey')
+export CONVERSATION_URL=$(echo "$service_watson_conversation" | jq -r '.url')
 npm start
